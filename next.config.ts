6e74d4c1763128f287edaf922ca9df857638b1df
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2025-05-14 08:42:52 +0300

    first version

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+// next.config.ts
+
+const nextConfig = {
+/*   experimental: {
+    // Required for Turbopack to transpile non-Next packages
+    // Especially if you're working with ESM-only packages
+    transpilePackages: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js'],
+  }, */
+};
+
+export default nextConfig;
